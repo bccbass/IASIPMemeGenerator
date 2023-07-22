44d8a090feb6b74cde11bbb760497a3f4488f4e6
Benjamin Campbell <benjamincampbell@benjamins-mini.lan>
2023-07-22 11:25:02 +1000

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const btn = document.querySelector('.img-wrapper')
+const quote = document.querySelector('#quote')
+const author = document.querySelector('#author')
+const img = document.querySelector('img')
+const imgWrapper = document.querySelector('.img-wrapper')
+
+const characters = {
+    Charlie: './media/Charlie.png',
+    Dennis: './media/Dennis.png',
+    Frank: './media/Frank.png',
+    Dee: './media/Dee.png',
+    Mac: './media/Mac.png',
+}
+
+const corsProxy = 'https://cors-anywhere.herokuapp.com/'
+const getQuote = async () => {
+    imgWrapper.classList.add('fade-out')    
+    const res = await fetch(corsProxy + 'http://sunnyquotes.net/q.php?random')
+     const quoteRes = await res.json()
+    quote.innerHTML = `"${quoteRes.sqQuote}"`
+    author.innerHTML = '- ' + quoteRes.sqWho
+    const who =  quoteRes.sqWho.split(' ')[0]
+    characters[who] ? img.src = characters[who] : img.src = './media/theGang.png'
+    imgWrapper.classList.remove('fade-out')    
+    imgWrapper.classList.add('fade-in')    
+
+    }
+
+
+
+
+btn.addEventListener('click', getQuote)
